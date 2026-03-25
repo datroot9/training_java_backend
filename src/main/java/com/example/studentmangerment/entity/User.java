d43e3609b2f861d66b72fbfc8d06d60bc51b2434
datroot9 <datht2@runsystem.net>
2026-03-25 09:54:27 +0000

    mapping user entity to table in db

--- src/main/java/com/example/studentmangerment/entity/User.java
+++ src/main/java/com/example/studentmangerment/entity/User.java
@@ -1,7 +1,12 @@
 package com.example.studentmangerment.entity;
 
 import lombok.*;
+import org.seasar.doma.Entity;
+import org.seasar.doma.Table;
 
+
+@Entity(immutable = true)
+@Table(name = "user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
