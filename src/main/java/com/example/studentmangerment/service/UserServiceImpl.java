ab581b7ba82f3255c6cde1f19c406e3c46942e74
datroot9 <datht2@runsystem.net>
2026-03-31 01:18:16 +0000

    apply DI to service layer

---
+++ src/main/java/com/example/studentmangerment/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.studentmangerment.service;
+
+import com.example.studentmangerment.dao.UserDao;
+import com.example.studentmangerment.dto.request.LoginRequest;
+import com.example.studentmangerment.dto.request.RegisterRequest;
+import com.example.studentmangerment.dto.response.AuthResponse;
+import com.example.studentmangerment.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userDao.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            throw new RuntimeException("Passwords do not match");
+        }
+//        //hash password
+        request.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .build();
+        userDao.insert(user);
+
+//        response for register request have no jwt
+        return AuthResponse.builder()
+                .username(user.getUsername())
+                .build();
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        User user = userDao.findByUsername(request.getUsername())
+                .orElse(null);
+        if (user != null && bCryptPasswordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return AuthResponse.builder()
+                    .id(user.getId())
+                    .username(user.getUsername())
+                    .token(null) // Will be replaced with JWT token in the future
+                    .build();
+        }
+        throw new RuntimeException("Invalid username or password");
+    }
+}
